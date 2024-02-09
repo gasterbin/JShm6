e00cc663f9e722f8c1323560d4b05ee814b5cc03
gasterbin <tergabi2007@gmail.com>
2024-02-09 23:35:30 +0200

    New Changes

--- src/index.js
+++ src/index.js
@@ -6,35 +6,25 @@
  */
 
 function calculateWinnerRating(winnerRating, opponentRating) {
-  if (
-    winnerRating < 0 ||
-    opponentRating < 0 ||
-    isNaN(winnerRating) ||
-    isNaN(opponentRating)
-  ) {
+  const ratingDifference = Math.abs(opponentRating - winnerRating);
+
+  if (isNaN(ratingDifference) || winnerRating < 0 || opponentRating < 0) {
     return NaN;
   }
 
-  if (winnerRating === 0) {
-    return opponentRating;
+  if (ratingDifference <= 2) {
+    return +(winnerRating + 2).toFixed(1);
   }
-
-  let ratingDifference = winnerRating - opponentRating;
-
-  if (ratingDifference >= 0 && ratingDifference <= 2) {
-    return parseFloat((winnerRating + 2).toFixed(1));
-  } else if (ratingDifference > 2 && ratingDifference < 20) {
-    return parseFloat((winnerRating + 1).toFixed(1));
-  } else if (ratingDifference >= 20) {
-    return parseFloat(winnerRating.toFixed(1)); // Не впевнена чи можна просто повернути winnerRating, чи потрібно округяти цю частину
-  } else if (ratingDifference < 0) {
-    return parseFloat(
-      (winnerRating + (opponentRating - winnerRating + 5) / 3).toFixed(1)
-    );
+  if (ratingDifference < 20) {
+    return +(winnerRating + 1).toFixed(1);
   }
+  if (winnerRating < opponentRating) {
+    return +(winnerRating + (ratingDifference + 5) / 3).toFixed(1);
+  }
+  return +winnerRating.toFixed(1);
 }
 
 const winnerRating = 1500;
 const opponentRating = 1400;
 const newRating = calculateWinnerRating(winnerRating, opponentRating);
-console.log(`Новий рейтинг переможця: ${newRating}`);
+console.log(`Новий рейтинг переможця: ${newRating}`); 
